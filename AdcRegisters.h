7cf4e19797470c78a26cd6aa2c5a106788419a81
Wouter Buurman <Wjbuurman2003@gmail.com>
2024-09-19 12:50:31 +0200

    Added div and cs registers

--- AdcRegisters.h
+++ AdcRegisters.h
@@ -2,11 +2,15 @@
 #define ADC_CS_OFFSET    (0x00)
 #define ADC_RESULT_OFFSET (0x04)
 #define ADC_DIV_OFFSET   (0x10)
+#define ADC_FCS_OFFSET (0x08)
+#define ADC_INTS_OFFSET (0x10)
 
 // Accessing the ADC registers
 #define ADC_CS     (*(volatile uint32_t *)(ADC_BASE + ADC_CS_OFFSET))
 #define ADC_RESULT (*(volatile uint32_t *)(ADC_BASE + ADC_RESULT_OFFSET))
 #define ADC_DIV    (*(volatile uint32_t *)(ADC_BASE + ADC_DIV_OFFSET))
+#define ADC_FCS    (*(volatile uint32_t *)(ADC_BASE + ADC_FCS_OFFSET))
+
 
 // Define bit masks for relevant fields in the ADC registers
 #define ADC_CS_EN          (1 << 0)
